336b2ee1b453d4fe5c8c63780dec5d607a427f33
Jamie Connolly <jamie@jamieconnolly.com>
2016-06-24 23:00:24 +0100

    Update pyenv-virtualenv-ext to 0.5.1

--- Formula/pyenv-virtualenv-ext.rb
+++ Formula/pyenv-virtualenv-ext.rb
@@ -1,8 +1,8 @@
 class PyenvVirtualenvExt < Formula
   desc "Make pyenv transparently aware of a virtual environment"
   homepage "https://github.com/jamieconnolly/pyenv-virtualenv-ext"
-  url "https://github.com/jamieconnolly/pyenv-virtualenv-ext/archive/0.5.0.tar.gz"
-  sha256 "6c9f5eb261e204220bbf5f5aa9e8aeef6eccead81121d45a5061295c5308fd8a"
+  url "https://github.com/jamieconnolly/pyenv-virtualenv-ext/archive/0.5.1.tar.gz"
+  sha256 "e2c3995eb035d95514bcc510fbfbf73a8f7440aa0649354c8a4a297209090c5e"
 
   head "https://github.com/jamieconnolly/pyenv-virtualenv-ext.git"
 
