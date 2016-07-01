398a65de5d7b3f6e1ebdff3a438aa21926686ce2
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-07-01 22:59:55 +0100

    Update pyenv-virtualenv-ext to 0.7.0

--- Formula/pyenv-virtualenv-ext.rb
+++ Formula/pyenv-virtualenv-ext.rb
@@ -1,8 +1,8 @@
 class PyenvVirtualenvExt < Formula
   desc "Make pyenv transparently aware of a virtual environment"
   homepage "https://github.com/jamieconnolly/pyenv-virtualenv-ext"
-  url "https://github.com/jamieconnolly/pyenv-virtualenv-ext/archive/0.6.0.tar.gz"
-  sha256 "d5476fd3fd2d1eeb862bae7d45e440b424e40035d6bcfc684d795314d773f362"
+  url "https://github.com/jamieconnolly/pyenv-virtualenv-ext/archive/0.7.0.tar.gz"
+  sha256 "cd4b7964cdf55c5602f450738b4615058424782b4e4aaf07e8a8b1fa9575150b"
 
   head "https://github.com/jamieconnolly/pyenv-virtualenv-ext.git"
 
@@ -17,7 +17,6 @@ class PyenvVirtualenvExt < Formula
   end
 
   test do
-    assert_match "virtualenv-ext.bash", shell_output("pyenv hooks version-name")
-    assert_match "virtualenv-ext.bash", shell_output("pyenv hooks version-origin")
+    assert_match "virtualenv-ext.bash", shell_output("pyenv hooks which")
   end
 end
