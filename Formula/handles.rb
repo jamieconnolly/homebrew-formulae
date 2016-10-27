168df66dd362d90afe515afe5d49a3faf7ecb1ec
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-10-27 15:14:24 +0100

    Update handles to 0.1

--- Formula/handles.rb
+++ Formula/handles.rb
@@ -1,9 +1,9 @@
 class Handles < Formula
   desc "My plain, docile robot buddy"
   homepage "https://github.com/jamieconnolly/handles"
-  url "https://github.com/jamieconnolly/handles/archive/e5efe0604e73bc7ee11ccdcf5cdad6ecabc323c4.tar.gz"
-  version "0.0.2"
-  sha256 "44a3ba2db9bcd29a6aedb1bf191e6116c38757995b3a4e44af01bc586ed947a0"
+  url "https://github.com/jamieconnolly/handles/archive/7044ee2f4e488ebb84f97f689aa9fe4272d30fa7.tar.gz"
+  version "0.1"
+  sha256 "e0eac65b26115dc52d7247260bc0b833728405f98443d0b748e26aaa60e77df7"
 
   head "https://github.com/jamieconnolly/handles.git"
 
@@ -15,10 +15,12 @@ class Handles < Formula
   def install
     ENV["GOPATH"] = buildpath
     ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
+
     (buildpath/"src/github.com/jamieconnolly/handles").install buildpath.children
     cd "src/github.com/jamieconnolly/handles" do
       system "glide", "install"
-      system "make", "build"
+      system "make", "build", "VERSION=#{version}"
+
       bin.install Dir["bin/*"]
       libexec.install Dir["libexec/*"]
       prefix.install_metafiles
@@ -26,6 +28,6 @@ class Handles < Formula
   end
 
   test do
-    assert_match "Hello, World!", shell_output("#{bin}/handles example")
+    assert_match "handles version #{version}", shell_output("#{bin}/handles --version")
   end
 end
