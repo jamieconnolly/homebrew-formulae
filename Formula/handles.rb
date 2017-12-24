129949f66a0efe8e3fda53a9b124e41eee9c20f7
Jamie Connolly <jamie@jamieconnolly.com>
2017-12-24 16:18:55 +0000

    Update handles to v21

--- Formula/handles.rb
+++ Formula/handles.rb
@@ -1,8 +1,8 @@
 class Handles < Formula
   desc "My plain, docile robot buddy"
   homepage "https://github.com/jamieconnolly/handles"
-  url "https://github.com/jamieconnolly/handles/archive/v20.tar.gz"
-  sha256 "04c5ca4ff5befcbbd229c99c3676d39a0bfeb3ad76d0cd847d5eaf0b8f63c9cd"
+  url "https://github.com/jamieconnolly/handles/archive/v21.tar.gz"
+  sha256 "5cc6836fa6db04e6a6e8a976209388eb67cb346ae860aad0bedd9b6aa310f6ca"
 
   head "https://github.com/jamieconnolly/handles.git"
 
